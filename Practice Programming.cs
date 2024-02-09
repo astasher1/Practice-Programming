08cd493cffa8d75bb821c2fbf1cc2a0e17b58192
astasher1 <159500293+astasher1@users.noreply.github.com>
2024-02-08 18:53:49 -0600

    Add files via upload

---
+++ Practice Programming.cs
@@ -0,0 +1,94 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, there!");
+Console.ReadLine();
+Console.WriteLine("We're going to practice coding today!");
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+//PRACTICE PROGRAMMING 
+//1. Personal Information - Design a program that displays the following information:
+//• Your name
+//• Your address, with city, state, and ZIP
+//• Your telephone number
+//• Your job title 
+
+Console.WriteLine("What is your name? ");
+string name;
+name = Console.ReadLine();
+
+Console.WriteLine("Hello, " + name);
+Console.ReadLine();
+
+Console.WriteLine("What is your address? (City, state, and ZIP)");
+string address;
+address = Console.ReadLine();
+
+Console.WriteLine("Phone number?");
+string number;
+number = Console.ReadLine();
+
+Console.WriteLine("And job title?");
+string jobTitle;
+jobTitle = Console.ReadLine();
+
+Console.WriteLine("Thank you, " + name + ". You just finished the first prompt! Let's move on.");
+Console.ReadLine();
+
+
+//2. Sales Prediction - A company has determined that its annual profit
+//is typically 23 percent of total sales. Design a program that asks
+//the user to enter the projected amount of total sales, and then displays the profit
+//that will be made from that amount. Hint: Use the value 0.23 to represent 23 percent.
+Console.WriteLine("This program is going to ask you to enter the projected amount of total sales for your company this year." +
+    "Enter total amount.");
+
+// 3. Land Calculation - One acre of land is equivalent to 43,560 square feet.
+// Design a program that asks the user to enter the total square feet in a tract
+// of land and calculates the number of acres in the tract.
+// Hint: Divide the amount entered by 43,560 to get the number of acres. 
+
+// 4. Sales Tax - Design a program that will ask the user to enter the amount
+// of a purchase. The program should then compute the state and county sales tax.
+// Assume the state sales tax is 4 percent and the county sales tax is 2 percent.
+// The program should display the amount of the purchase, the state sales tax,
+// the county sales tax, the total sales tax, and the total of the sale
+// (which is the sum of the amount of purchase plus the total sales tax).
+// Hint: Use the value 0.02 to represent 2 percent, and 0.04 to represent 4 percent 
+
+//5. Total Purchase - A customer in a store is purchasing five items.
+//Design a program that asks for the price of each item, and then displays
+//the subtotal of the sale, the amount of sales tax, and the total. Assume
+//the sales tax is 6 percent.
+
+// 6. Distance Traveled - Assuming there are no accidents or delays,
+// the distance that a car travels down the interstate can be calculated
+// with the following formula:
+// Distance = Speed × Time A car is traveling at 60 miles per hour.
+// Design a program that displays the following:
+// • The distance the car will travel in 5 hours
+// • The distance the car will travel in 8 hours
+// • The distance the car will travel in 12 hours 
+
+//7. Miles-per-Gallon - A car’s miles-per-gallon (MPG) can be calculated
+//with the following formula: MPG = Miles driven / Gallons of gas used
+//Design a program that asks the user for the number of miles driven and
+//the gallons of gas used. It should calculate the car’s miles-per-gallon
+//and display the result on the screen. 
+
+// 8. Tip, Tax, and Total - Design a program that calculates the total amount
+// of a meal purchased at a restaurant. The program should ask the user to enter
+// the charge for the food, and then calculate the amount of a 15 percent tip and
+// 7 percent sales tax. Display each of these amounts and the total. 
+
+// 9. Celsius to Fahrenheit - Temperature Converter - Design a program that converts
+// Celsius temperatures to Fahrenheit temperatures. The program should ask the user
+// to enter a temperature in Celsius, and then display the temperature converted to
+// Fahrenheit.
+
